d5ea625dcba1a886eb58d886890b4873c9201b4d
Haisu Zuo <zuohaisu@gmail.com>
2013-09-02 22:17:22 +0800

    running version

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+  	@welcomeMsg = "Welcom to Haisu Zuo's learning page"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '1c31fd7d087824f5b8391b84898d5e52177470e53a58dcc7df9b75d9f19b5396a4c2e209577e5ad93f5a05beca97acf5957e5a5c81cff7a0812477e1d6646e4a'
